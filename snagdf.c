160b7f88f45ab60b3831e83bc54d1b8787de662d
Anthony de Boer <adb@lagavulin>
2010-11-23 23:07:45 -0500

    Use getmntent (Linux) or getfsstat (BSD) as appropriate

--- snagdf.c
+++ snagdf.c
@@ -17,14 +17,11 @@
  *	USA
  */
 
-#include <stdio.h>
-#include <mntent.h>
-#include <sys/statvfs.h>
-#include <string.h>
-#include <stdlib.h>
 #include "snag.h"
 
 #define debug 0
+
+void printdisk(char *mntdir, unsigned long blocksize, unsigned long tblocks, unsigned long avblocks, unsigned long tinodes, unsigned long avinodes);
 
 /* Convert to percentage, making sure we don't run into arithmetic
  * overflow.
@@ -37,6 +34,9 @@ int hundiv (unsigned long numer, unsigned long denom) {
 	return 100 * numer / denom;
 	}
 
+#ifdef HAVE_GETMNTENT
+#include <mntent.h>
+#include <sys/statvfs.h>
 int snagdf () {
 	FILE *mntf;
 	struct mntent *ment;
@@ -60,42 +60,9 @@ int snagdf () {
 			else {
 				if (debug) fprintf(stderr, "got data\n");
 				if (debug) fprintf(stderr, "bavail %lu blocks %lu favail %lu files %lu bsize %lu\n", vfs.f_bavail, vfs.f_blocks, vfs.f_favail, vfs.f_files, vfs.f_bsize);
-				int col;
-				char *sdup = strdup(ment->mnt_dir);
-				char *show;
-				if (!sdup) {
-					show = "NULL";
-					}
-				else {
-					show = sdup;
-					while(*show) {
-						if (!isalnum(*show) && *show != '/') {
-							*show = '_';
-							}
-						show++;
-						}
-					show = sdup;
-					while(*show == '_') {
-						show++;
-						}
-					if (!*show) {
-						show = "root";
-						}
-					}
-				if (vfs.f_blocks && vfs.f_bsize) {
-					int bfree = hundiv(vfs.f_bavail, vfs.f_blocks);
-					unsigned long megavail = vfs.f_bavail / (1048576 / vfs.f_bsize);
-					col = thresher("Disk_space", bfree);
-					printf("Disk space on %s;%d;DISK %s - %s %lu MB (%d%%) free space|%s=%luMB\n", show, col, statusword(col), ment->mnt_dir, megavail, bfree, ment->mnt_dir, megavail);
-					}
-				if (vfs.f_files) {
-					int ifree = hundiv(vfs.f_favail, vfs.f_files);
-					unsigned long iavail = vfs.f_favail;
-					col = thresher("Disk_inodes", ifree);
-					printf("Disk inodes on %s;%d;INODES %s - %s %lu inodes (%d%%) free|%s=%lu inodes\n", show, col, statusword(col), ment->mnt_dir, iavail, ifree, ment->mnt_dir, iavail);
-					}
-				free(sdup);
-
+				printdisk(ment->mnt_dir, vfs.f_bsize,
+					vfs.f_blocks, vfs.f_bavail,
+					vfs.f_files, vfs.f_favail);
 				}
 			}
 		}
@@ -103,3 +70,87 @@ int snagdf () {
 	endmntent(mntf);
 	return(rc);
 	}
+#else
+
+#ifdef HAVE_GETFSSTAT
+
+#include <sys/param.h>
+#include <sys/mount.h>
+
+int snagdf () {
+	size_t n1, n2, bs;
+	struct statfs * fsbuf, * fscur;
+	int i;
+
+	n1 = getfsstat(NULL, 0, MNT_WAIT);
+	if (debug) fprintf(stderr, "fs count %d\n", n1);
+	if (n1 < 1) return 0;
+	bs = n1 * sizeof(struct statfs);
+ 	fsbuf = (struct statfs *)malloc(bs);
+	if (fsbuf == NULL) {
+		fprintf(stderr, "statfs alloc failed\n");
+		return 2;
+		}
+	if ((n2 = getfsstat(fsbuf, bs, MNT_WAIT)) == -1) {
+		perror("getfsstat");
+		return 2;
+		}
+	fscur = fsbuf;
+	if (debug && n1 != n2) fprintf(stderr, "n1 %d n2 %d\n", n1, n2);
+	for (i=0; i<n2; i++) {
+		if (!(fscur->f_flags & MNT_RDONLY) &&
+			fscur->f_mntfromname[0] == '/' &&
+			fscur->f_mntfromname[1] == 'd' &&
+			fscur->f_mntfromname[2] == 'e' &&
+			fscur->f_mntfromname[3] == 'v' &&
+			fscur->f_mntfromname[4] == '/')
+		printdisk(fscur->f_mntonname, fscur->f_bsize,
+			fscur->f_blocks, fscur->f_bavail,
+			fscur->f_files, fscur->f_ffree);
+		fscur++;
+	}
+	free(fsbuf);
+	return 0;
+}
+#else
+#error No snagdf implementation
+#endif /* HAVE_GETFSSTAT */
+#endif /* HAVE_GETMNTENT */
+
+void printdisk(char *mntdir, unsigned long blocksize, unsigned long tblocks, unsigned long avblocks, unsigned long tinodes, unsigned long avinodes)
+{
+	int col;
+	char *sdup = strdup(mntdir);
+	char *show;
+	if (sdup == NULL) {
+		show = "NULL";
+	} else {
+		show = sdup;
+		while(*show) {
+			if (!isalnum(*show) && *show != '/') {
+				*show = '_';
+				}
+			show++;
+		}
+		show = sdup;
+		while(*show == '_') {
+			show++;
+		}
+		if (!*show) {
+			show = "root";
+		}
+	}
+	if (tblocks && blocksize) {
+		int bfree = hundiv(avblocks, tblocks);
+		unsigned long megavail = avblocks / (1048576 / blocksize);
+		col = thresher("Disk_space", bfree);
+		printf("Disk space on %s;%d;DISK %s - %s %lu MB (%d%%) free space|%s=%luMB\n", show, col, statusword(col), mntdir, megavail, bfree, mntdir, megavail);
+	}
+	if (tinodes) {
+		int ifree = hundiv(avinodes, tinodes);
+		unsigned long iavail = avinodes;
+		col = thresher("Disk_inodes", ifree);
+		printf("Disk inodes on %s;%d;INODES %s - %s %lu inodes (%d%%) free|%s=%lu inodes\n", show, col, statusword(col), mntdir, iavail, ifree, mntdir, iavail);
+	}
+	if (sdup) free(sdup);
+}
