967438f2bc7f2fa898e797bceb1e17adc017be0a
Anthony de Boer <adb@adb.ca>
2011-01-02 22:13:18 -0500

    DTRT when facing a ZFS mountpoint

--- snagdf.c
+++ snagdf.c
@@ -33,9 +33,26 @@ int hundiv (unsigned long numer, unsigned long denom) {
 	}
 
 /*
+ * filesystem type rules:
+ * 0 to hide, 1 to show, 2 to show without inodes.
+ * No need for OS-specific hacks unless the same filesystem name
+ * gets used for sufficiently different things in different places.
+ */
+
+static struct fstable_t {
+	char *fstname;
+	int showit;
+	} fstable[] = {
+		{ "nfs", 0 },
+		{ "zfs", 2 },
+		{ "swap", 0 },
+		{ NULL, 0 },
+	};
+/*
  * Handle one mountpoint:
  * mntdir - path to mountpoint
  * mntdev - device name
+ * mnttype - filesystem type string
  * funny - flags like read-only that indicate to probably skip this mount
  * blocksize
  * tblocks - total blocks
@@ -44,9 +61,17 @@ int hundiv (unsigned long numer, unsigned long denom) {
  * avinodes - number of inodes free
  */
 
-void printdisk(char *mntdir, char *mntdev, int funny, unsigned long blocksize, unsigned long tblocks, unsigned long avblocks, unsigned long tinodes, unsigned long avinodes)
+void printdisk(char *mntdir, char *mntdev, char *mnttype, int funny, unsigned long blocksize, unsigned long tblocks, unsigned long avblocks, unsigned long tinodes, unsigned long avinodes)
 {
 	int col;
+
+	if (debug) {
+		printf("Consider DIR=%s, DEV=%s TYPE=%s (%s)\n",
+			mntdir ? mntdir : "NULL",
+			mntdev ? mntdev : "NULL",
+			mnttype ? mnttype : "NULL",
+			funny ? "FUNNY" : "NORMAL");
+	}
 
 	char *sdup = strdup(mntdir);
 	if (sdup == NULL) {
@@ -71,12 +96,16 @@ void printdisk(char *mntdir, char *mntdev, int funny, unsigned long blocksize, u
 	int specific = (thresher(show, 12345) < 3);
 	int showme = specific;
 
-	if (!specific) {
-		/* check if mntdev is other than /dev/...? */
-		if (funny || *mntdev != '/' || getvar(show)) {
-			showme = 0;
-		} else {
-			showme = 1;
+	if (!specific && !funny && !getvar(show)) {
+		struct fstable_t * fst;
+		/* default: check if mntdev is other than /dev/...? */
+		showme = mntdev != NULL && strncmp(mntdev, "/dev/", 5) == 0;
+		/* unless filesystem type is known */
+		for (fst = &fstable; fst->fstname; fst++) {
+			if (strcmp(fst->fstname, mnttype) == 0) {
+				showme = fst->showit;
+				break;
+			}
 		}
 	}
 
@@ -86,7 +115,7 @@ void printdisk(char *mntdir, char *mntdev, int funny, unsigned long blocksize, u
 		col = thresher(specific ? show : "Disk_space", bfree);
 		printf("Disk space on %s;%d;DISK %s - %s %lu MB (%d%%) free space|%s=%luMB\n", show, col, statusword(col), mntdir, megavail, bfree, mntdir, megavail);
 	}
-	if (showme && tinodes) {
+	if (showme == 1 && tinodes) {
 		int ifree = hundiv(avinodes, tinodes);
 		col = thresher(specific ? show : "Disk_inodes", ifree);
 		printf("Disk inodes on %s;%d;INODES %s - %s %lu inodes (%d%%) free|%s=%lu inodes\n", show, col, statusword(col), mntdir, avinodes, ifree, mntdir, avinodes);
@@ -117,7 +146,7 @@ int snagdf () {
 		int funny = (hasmntopt(ment, "ro") != NULL) ||
 			(hasmntopt(ment, "bind") != NULL);
 		if (debug) fprintf(stderr, "bavail %lu blocks %lu favail %lu files %lu bsize %lu\n", vfs.f_bavail, vfs.f_blocks, vfs.f_favail, vfs.f_files, vfs.f_bsize);
-		printdisk(ment->mnt_dir, ment->mnt_fsname,
+		printdisk(ment->mnt_dir, ment->mnt_fsname, ment->mnt_type,
 			funny, vfs.f_bsize,
 			vfs.f_blocks, vfs.f_bavail,
 			vfs.f_files, vfs.f_favail);
@@ -157,6 +186,7 @@ int snagdf ()
 	for (i=0; i<n2; i++) {
 		int funny = (fscur->f_flags & MNT_RDONLY);
 		printdisk(fscur->f_mntonname, fscur->f_mntfromname,
+			fscur->f_fstypename,
 			funny, fscur->f_bsize,
 			fscur->f_blocks, fscur->f_bavail,
 			fscur->f_files, fscur->f_ffree);
